bcf7f10a115cd39b5e228ec59216c53a718717ad
tarin <tarinakter567@gmail.com>
2023-10-15 13:12:19 +0600

    add comment

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,6 +1,9 @@
 import 'package:dart_practice/dart_practice.dart' as dart_practice;
 
 void main() {
+ /*
+  This is simple code for print name using String datatype and also print that name
+ */
  String name = "Afsana Akter";
  print("My name is $name");
 }
