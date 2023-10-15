a5219e4460a18d57cb96ac891243389a596b861f
tarin <tarinakter567@gmail.com>
2023-10-15 13:19:14 +0600

    print single and double quotes

---
+++ bin/print_singel_double_quotes.dart
@@ -0,0 +1,8 @@
+void main(){
+  //This code for print simple single and double quotes String
+  String singleQuote = 'Hello I am "Jone Doe"';
+  String doubleQuote = 'Hello I\'am "Jone Doe" ';
+
+  print(singleQuote);
+  print(doubleQuote);
+}
