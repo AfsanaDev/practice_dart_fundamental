38c58e0ca43d683c8888355211bbfa1605190c97
tarin <tarinakter567@gmail.com>
2024-01-06 08:09:59 +0600

    practice replaceRange() function.

--- bin/listPractice.dart
+++ bin/listPractice.dart
@@ -1,5 +1,6 @@
 void main(){
   List names =['Afsana','Tarin','Shahin','Tuhin','Shirin'];
+  var newNames =['Sumi','akhi'];
   print('Names are $names');
   print(names[2]);
   for( int i =0 ; i<names.length; i++){
@@ -10,4 +11,6 @@ void main(){
   print(names);
   print(names.first);
   print(names.iterator);
+  names.replaceRange( 6,6, newNames);
+  print(names);
 }
